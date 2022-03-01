b03d68e3c0202ba43c8c93d8b95719d1fc3eadf6
Jill Cunningham <jleighcunningham@gmail.com>
2022-03-01 00:19:42 -0500

    added if statements and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,38 @@
-// Assignment code here
+// declare variables
+var randomLowercase = ["abcdefghijklmnopqrstuvwxyz"]
+var randomUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var randomNumeric = ["0123456789"]
+var randomSpecialCharacters = ["@#$%^&*()+<>/="]
+var randomPassword = randomLowercase + randomUppercase + randomNumeric + randomSpecialCharacters
 
-//Function to generate combination of password //
+//prompt for password length
 function generatePassword() {
-  var pass = '';
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?';
-    
-  for (i = 1; i <= 8; i++) {
-      var char = Math.floor(Math.random()
-                  * str.length + 1);
-        
-      pass += str.charAt(char)
+var passwordLength = prompt("How long would you like your password to be? Please be advised it must be 8 characters at min. and not exceed 128 characters.");
+
+//prompt for password length not being met//
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    alert("Your password is either less than 8 characters or more than 128 characters. Please attempt to re-enter a password that is 8 characters at min. or no more than 128 characters to proceed!");
+    return generatePassword();
   }
-    
-  return pass;
-}
-function gfg_Run() {
-  el_down.innerHTML = generatePassword();
+  //prompts for password criteria
+  if (passwordLength >=8 && passwordLength <= 128) {
+  //prompt for password lowercase
+    var randomLowercase = confirm("Click OK if you would like to include lowercase letters in your password or click Cancel to set other conditions.");
+  //prompt for uppercase
+    var randomUppercase = confirm("Click OK if you would like to include uppercase letters in your password or click Cancel to set other conditions.");
+  //prompt for numeric value
+    var randomNumeric= confirm("Click OK if you would like to include numeric values in your password or click Cancel to set other conditions.");
+  //prompt for special character
+    var randomSpecialCharacters = confirm("Click OK if you would like to include special characters in your password or click Cancel to set other conditions.");
+  }
+
+//for loop
+var password = "";
+  for (var i =0; i < passwordLength; i++) {
+    var trueRandom = randomPassword[Math.floor(Math.random() * randomPassword.length)];
+    password += trueRandom;
+  }
+return password
 }
 
 // Get references to the #generate element
