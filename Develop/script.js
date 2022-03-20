b73101b49edc71699e01ea924c858d9f06e76de3
Jill Cunningham <jleighcunningham@gmail.com>
2022-03-19 22:08:12 -0400

    updated readme and created images folder to hold webpage screenshots

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,52 @@
 // declare variables
-var randomLowercase = ["abcdefghijklmnopqrstuvwxyz"]
-var randomUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var randomNumeric = ["0123456789"]
-var randomSpecialCharacters = ["@#$%^&*()+<>/="]
-var randomPassword = randomLowercase + randomUppercase + randomNumeric + randomSpecialCharacters
+var randomLowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var randomUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var randomNumeric = ["0123456789"];
+var randomSpecialCharacters = ["@#$%^&*()+<>/="];
+var randomPassword =
+  randomLowercase + randomUppercase + randomNumeric + randomSpecialCharacters;
 
 //prompt for password length
 function generatePassword() {
-var passwordLength = prompt("How long would you like your password to be? Please be advised it must be 8 characters at min. and not exceed 128 characters.");
+  var passwordLength = prompt(
+    "How long would you like your password to be? Please be advised it must be 8 characters at min. and not exceed 128 characters."
+  );
 
-//prompt for password length not being met//
-  if (passwordLength < 8 || passwordLength > 128 ) {
-    alert("Your password is either less than 8 characters or more than 128 characters. Please attempt to re-enter a password that is 8 characters at min. or no more than 128 characters to proceed!");
+  //prompt for password length not being met//
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert(
+      "Your password is either less than 8 characters or more than 128 characters. Please attempt to re-enter a password that is 8 characters at min. or no more than 128 characters to proceed!"
+    );
     return generatePassword();
   }
   //prompts for password criteria
-  if (passwordLength >=8 && passwordLength <= 128) {
-  //prompt for password lowercase
-    var randomLowercase = confirm("Click OK if you would like to include lowercase letters in your password or click Cancel to set other conditions.");
-  //prompt for uppercase
-    var randomUppercase = confirm("Click OK if you would like to include uppercase letters in your password or click Cancel to set other conditions.");
-  //prompt for numeric value
-    var randomNumeric= confirm("Click OK if you would like to include numeric values in your password or click Cancel to set other conditions.");
-  //prompt for special character
-    var randomSpecialCharacters = confirm("Click OK if you would like to include special characters in your password or click Cancel to set other conditions.");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    //prompt for password lowercase
+    var randomLowercase = confirm(
+      "Click OK if you would like to include lowercase letters in your password or click Cancel to set other conditions."
+    );
+    //prompt for uppercase
+    var randomUppercase = confirm(
+      "Click OK if you would like to include uppercase letters in your password or click Cancel to set other conditions."
+    );
+    //prompt for numeric value
+    var randomNumeric = confirm(
+      "Click OK if you would like to include numeric values in your password or click Cancel to set other conditions."
+    );
+    //prompt for special character
+    var randomSpecialCharacters = confirm(
+      "Click OK if you would like to include special characters in your password or click Cancel to set other conditions."
+    );
   }
 
-//for loop
-var password = "";
-  for (var i =0; i < passwordLength; i++) {
-    var trueRandom = randomPassword[Math.floor(Math.random() * randomPassword.length)];
+  //for loop
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var trueRandom =
+      randomPassword[Math.floor(Math.random() * randomPassword.length)];
     password += trueRandom;
   }
-return password
+  return password;
 }
 
 // Get references to the #generate element
@@ -44,7 +58,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Table of Contents
+
+- [Description](#description)
+- [Features](#features)
+- [User-Story](#user-story)
+- [Acceptance-Criteria](#acceptance-criteria)
+- [Usage](#usage)
+- [Webpage-Screenshots](#webpage-screenshots)
+- [Github-Link](#github-link)
+- [Deployed-Link](#deployed-link)
+- [Contributor](#contributor)
 
 ## Description
+
+This application assists users in generating a strong, unique, and secure password based on their input.
+
+## Features:
+
+This application was built with:
+
+- HTML
+- CSS
+- Javascript
+
+## User Story
 
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
@@ -24,10 +48,51 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 
-## Screenshots
+## Usage
 
-## Webpage Links
+- Users will click 'Generate Password. Then, they will be prompted with several inquiries to assist with generating a strong password. 'Ok' can be clicked if the user agrees to the password condition and 'Cancel' if they do not want that condition to be incorportated in their password.
 
-### Deployed Link
+The following conditions are prompts that the users will see to assist with password generation:
+(a) choice of a character limit from 8 to 128,
+(b) lowercase or uppercase letters,
+(c) numeric values,
+(d) special characters
 
-### GitHub URL
+Once these conditions are addressed, then a password that meets those conditions will be rendered to the webpage.
+
+## Webpage Screenshots
+
+An example of the password generator homepage:
+![Screenshot](<assets/images/Screenshot%20(10).png>)
+
+An example of the character limit condition:
+![Screenshot](<assets/images/Screenshot%20(11).png>)
+
+An example of the lowercase condition:
+![Screenshot](<assets/images/Screenshot%20(12).png>)
+
+An example of the lowercase condition:
+![Screenshot](<assets/images/Screenshot%20(13).png>)
+
+An example of the numeric values condition:
+![Screenshot](<assets/images/Screenshot%20(14).png>)
+
+An example of the special character condition:
+![Screenshot](<assets/images/Screenshot%20(15).png>)
+
+An example of the generated password when all conditions are requested:
+![Screenshot](<assets/images/Screenshot%20(16).png>)
+
+## GitHub URLs
+
+https://github.com/jlcunningham2101/password-generator
+
+## Deployed Link
+
+https://jlcunningham2101.github.io/password-generator/
+
+## Contributor
+
+Name: Jill Cunningham
+Email: jleighcunningham@gmail.com
+GitHub URL: https://github.com/jlcunningham2101
